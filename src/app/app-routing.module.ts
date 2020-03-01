44ecbec44e7e4ec9fbdd588775bba8e4823d1d49
Pittan <pittan@pittankopta.net>
2020-03-01 23:12:36 +0900

    Add scroll restore function

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -37,7 +37,9 @@ const routes: Routes = [
 ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    scrollPositionRestoration: 'enabled'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
